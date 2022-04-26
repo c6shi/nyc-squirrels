c091915d0ff3525ab1f6fa7531405d3da2c90a22
Candus Shi <candusshi@gmail.com>
2022-04-26 09:13:28 -0700

    added graphs and csv of potential buffer formulas

--- data cleaning (py).py
+++ data cleaning (py).py
@@ -33,4 +33,34 @@ lat_float = nyc_gdf['lat'].apply(lambda x: float(x))
 
 nyc_gdf1 = nyc_gdf1.assign(long=nyc_gdf1['long'].apply(lambda x: float(x)),
                            lat=nyc_gdf['lat'].apply(lambda x: float(x)))
-print("hello")
+
+# drop unnecessary columns
+
+trash = ['lat_long', 'community_districts', 'borough_boundaries',
+         'city_council_districts', 'police_precincts']
+nyc_gdf2 = nyc_gdf1.drop(columns=trash)
+print(nyc_gdf2.columns)
+
+# load geojson datasets
+
+centralpark_water_osm = gpd.read_file('geojson/water_cp.geojson')
+centralpark_stream_osm = gpd.read_file('geojson/streams_cp.geojson')
+centralpark_playg_osm = gpd.read_file('geojson/playgrounds_cp.geojson')
+centralpark_paved_osm = gpd.read_file('geojson/paved.geojson')
+centralpark_toilets_osm = gpd.read_file('geojson/toilets.geojson')
+cp_northbd_osm = gpd.read_file('geojson/northbd.geojson')  # cp north
+cp_jorbd_osm = gpd.read_file('geojson/jorbd.geojson')  # jacqueline kennedy onassis reservoir
+cp_greatlawnbd_osm = gpd.read_file('geojson/greatlawnbd.geojson')  # great lawn
+cp_ramblebd_osm = gpd.read_file('geojson/ramblebd.geojson')  # ramble
+cp_southbd_osm = gpd.read_file('geojson/southbd.geojson')  # cp south
+met_osm = gpd.read_file('geojson/met.geojson')  # the met
+centralpark_gardens_osm = gpd.read_file('geojson/gardens_cp.geojson')
+allwoodsosm = gpd.read_file('geojson/woods_cp.geojson')
+cp_baseball_osm = gpd.read_file('geojson/baseballpitch.geojson')
+cp_field_osm = gpd.read_file('geojson/field.geojson')
+allbarerockosm = gpd.read_file('geojson/bare_rock.geojson')
+cp_pedestrian_osm = gpd.read_file('geojson/pedestrian.geojson')
+cp_sportscenter_osm = gpd.read_file('geojson/sportscenter.geojson')
+
+# data cleaning
+
