7ee8dc6bf63fa0fea04c6618f2896f2978df66a3
Candus Shi <candusshi@gmail.com>
2022-05-18 12:53:44 -0700

    results discussion page updated

--- apps/resultsdisc.py
+++ apps/resultsdisc.py
@@ -6,10 +6,26 @@ import plotly.express as px
 def app():
     st.title("Discussion of Results")
 
+    st.markdown(
+        """
+        Here is a table of the p-values for all the combinations we conducted a permutation test on.
+        """
+    )
+
     results = pd.read_csv('dataframes/permutation_results.csv')
     results = results.drop(columns='Unnamed: 0')
-
     sig = results[results['p-value'] < 0.05]
+
+    sorted_by_p = sig.sort_values(by='p-value')
+    hide_dataframe_row_index = """
+                        <style>
+                        .row_heading.level0 {display:none}
+                        .blank {display:none}
+                        </style>
+                        """
+    st.markdown(hide_dataframe_row_index, unsafe_allow_html=True)
+    st.dataframe(sorted_by_p)
+
     alluvial = px.parallel_categories(
         sig,
         dimensions=['behavior', 'feature 1', 'feature 2'],
@@ -17,4 +33,26 @@ def app():
         color_continuous_scale=px.colors.sequential.Oryel_r,
     )
     st.plotly_chart(alluvial)
+
+    st.markdown(
+        """
+        The interactive alluvial chart above helps visualize which behavior and feature was most common 
+        and most significant. Feel free to drag the unique values for each variable to see the specific combinations
+        more clearly.
+        """
+    )
+
+    st.subheader("Observations")
+    st.markdown(
+        """
+        - The `indifferent` behavior was the most common behavior that had a significant result. 
+            - 8 out of the 18 significant combinations (~44.4%) had `indifferent` as the behavior.
+            - Among the indifferent combinations, 5 out of the 8 had `neargrass` as a feature.
+            - The 4 most statistically significant combinations had the `indifferent` behavior and `neargrass` feature. 
+        - The `nearwoods` feature was the most common feature that had a significant result.
+            - 10 out of the 18 significant combinations (~55.6%) had `nearwoods` as one of the features.
+        - The `neargrass` feature was the next most common feature that had a significant result.
+            - 9 out of the 18 significant combinations (50%) had `neargrass` as one of the features.
+        """
+    )
 
